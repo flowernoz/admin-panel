4c2a9d16960824c9193d5b2b662e2dea21ce73a8
Flowernoza <cadillac2111@Salmoorbeks-MacBook-Pro.local>
2024-01-18 23:28:54 +0500

    ..

--- src/routes/admin/sidebar/Sidebar.jsx
+++ src/routes/admin/sidebar/Sidebar.jsx
@@ -1,14 +1,31 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 import { HiBars3 } from "react-icons/hi2";
 import { MdClose } from "react-icons/md";
 import logoutImg from "../../../assets/admin/log_out.png";
 import "./Sidebar.css";
 import { useState } from "react";
 import ModalBar from "./ModalBar";
-function Sidebar({ logout }) {
+import { toast, ToastContainer } from "react-toastify";
+function Sidebar() {
   const [openModal, setOpenModal] = useState(false);
+  let navigate = useNavigate();
+
+  function logout() {
+    let confirmation = window.confirm("Do you really want to log out?");
+    if (confirmation) {
+      localStorage.removeItem("admin");
+      toast.success("Tizimdan muvaffaqiyatli chiqildi", {
+        position: "top-center",
+        autoClose: 1500,
+        hideProgressBar: true,
+      });
+      setTimeout(() => navigate("/adminlogin"), 1500);
+    }
+  }
+
   return (
     <div className="admin__sidebar">
+      <ToastContainer />
       {openModal && <ModalBar setOpenModal={setOpenModal} />}
       <div className="sidebar__logo">
         <div></div>
